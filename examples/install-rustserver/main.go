51074c8c1aff7f7f463648b40066c88308d53b22
Jens van de Wiel <jens.vdwiel@gmail.com>
2023-08-03 23:52:37 +0200

    updated example

--- examples/install-rustserver/main.go
+++ examples/install-rustserver/main.go
@@ -1,15 +1,18 @@
 package main
 
-import "github.com/jensvandewiel/gosteamcmd"
+import (
+	"github.com/jensvandewiel/gosteamcmd"
+	"os"
+)
 
 func main() {
 	//this code follows the steps of: https://www.rustafied.com/how-to-host-your-own-rust-server
-	cmd := gosteamcmd.New()
-	cmd.Prompts = append(cmd.Prompts, gosteamcmd.ForceInstallDir("c:\\rustserver\\"))
+	cmd := gosteamcmd.New(os.Stdout)
+	cmd.Prompts = append(cmd.Prompts, gosteamcmd.ForceInstallDir("c:\\obamaserver\\"))
 	cmd.Prompts = append(cmd.Prompts, gosteamcmd.Login("", ""))
 	cmd.Prompts = append(cmd.Prompts, gosteamcmd.AppUpdate(258550, "", false))
 	//running it headless means it will not output anything
-	err := cmd.RunHeadless()
+	err := cmd.Run()
 
 	if err != nil {
 		panic(err)
