aa71a78bccea91b454d8de3ae6f347dd532865c1
Jens van de Wiel <jens.vdwiel@gmail.com>
2023-08-03 20:28:44 +0200

    created AppUpdatePrompt and LoginPrompt

---
+++ propmt.go
@@ -0,0 +1,39 @@
+package gosteamcmd
+
+import "strconv"
+
+type PromptType int
+
+const (
+	AppUpdatePrompt PromptType = iota
+	LoginPrompt
+)
+
+type Prompt struct {
+	FullPrompt string
+	Type       PromptType
+}
+
+// AppUpdate updates the given appID. If beta is not empty, it will update to the given beta. If validate is true, it will validate the files.
+func AppUpdate(appID int, beta string, validate bool) Prompt {
+	cmd := "app_update "
+	cmd += strconv.Itoa(appID) + " "
+	if beta != "" {
+		cmd += "-beta " + beta + " "
+	}
+	if validate {
+		cmd += "validate"
+	}
+	return Prompt{cmd, AppUpdatePrompt}
+}
+
+// Login logs into SteamCMD with the given username and password. If the arguments are empty, it will login as anonymous.
+func Login(username string, password string) Prompt {
+	cmd := "login "
+	if username != "" {
+		cmd += username + " " + password
+	} else {
+		cmd += "anonymous"
+	}
+	return Prompt{cmd, LoginPrompt}
+}
